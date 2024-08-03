722adb8a7466ef727b361388cd04c2669ba96a71
parbez <imranbarbhuiya.fsd@gmail.com>
2024-08-03 19:01:24 +0530

    chore: format files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
-export default [...compat.extends("./src/common", "./src/node"), {
-    ignores: [".yarn/*"]
-}];
+export default [
+	...compat.extends('./src/common', './src/node'),
+	{
+		ignores: ['.yarn/*'],
+	},
+];
